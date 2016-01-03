33eaa765cc0303807ba1bcc622cbce7427c495f3
Eduard Roccatello <eduard.roccatello@3dgis.it>
2016-01-03 22:23:02 +0100

    database progress

--- src/termostato/data/db.py
+++ src/termostato/data/db.py
@@ -4,28 +4,40 @@ db = SqliteDatabase('termostato.db')
 
 
 class Reading(Model):
+    id = PrimaryKeyField()
     timestamp = DateTimeField()
     temperature = DoubleField()
-    started = BooleanField()
+    status = BooleanField()
 
     class Meta:
         database = db
+        order_by = ('timestamp')
 
 
 class Scheduling(Model):
+    id = PrimaryKeyField()
     dotw = IntegerField()
-    starthour = IntegerField()
-    startminute = IntegerField()
-    endhour = IntegerField()
-    endminute = IntegerField()
-    temperature = DoubleField()
+    start_time = TimeField()
+    end_time = TimeField()
 
     class Meta:
         database = db
 
 
-class BaseSettings(Model):
-    nighttemperature = DoubleField()
+class State(Model):
+    id = PrimaryKeyField()
+    name = CharField()
 
     class Meta:
         database = db
+
+
+class Setting(Model):
+    id = PrimaryKeyField()
+    day_temperature = DoubleField()
+    night_temperature = DoubleField()
+    state = ForeignKeyField(State)
+
+    class Meta:
+        database = db
+        auto_increment = False
